25547b86bf18ff7e24e776e00c8e8e1bebb57240
ariel almelda <arielalmelda@SnL-aalmelda.ameeratel.net>
2018-02-19 19:04:13 +0800

    this is new commeted files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 import Header from './common/header';
 import Body from './common/body';
+import './App.css';
 
 class App extends Component {
   render() {
--- src/common/header.js
+++ src/common/header.js
@@ -5,19 +5,23 @@ import { Link } from 'react-router-dom';
 class Header extends Component{
     render(){
         return(
-            <nav>
-                <li> 
-                    <Link to="/">Home</Link> 
-                </li>
-                <li> 
-                    <Link to="about-me">About Me</Link> 
-                </li>
-                <li> 
-                    <Link to="sample">Sample</Link> 
-                </li>
-                <li> 
-                    <Link to="contact-me">Contact Me</Link> 
-                </li>
+            <nav className="navbar navbar-default">
+                <div className="collapse navbar-collapse">
+                    <ul className="nav navbar-nav">
+                        <li> 
+                            <Link className="active" activeClassName="active" activeOnlyWhenExact to="/">Home</Link> 
+                        </li>
+                        <li> 
+                            <Link className="active" activeClassName="active" activeOnlyWhenExact to="about-me">About Me</Link> 
+                        </li>
+                        <li> 
+                            <Link className="active" activeClassName="active" activeOnlyWhenExact to="sample">Sample</Link> 
+                        </li>
+                        <li> 
+                            <Link className="active" activeClassName="active" activeOnlyWhenExact to="contact-me">Contact Me</Link> 
+                        </li>
+                    </ul>
+                </div>
             </nav>
         );
     }
