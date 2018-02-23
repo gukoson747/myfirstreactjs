fbad3d94dc0524f00526aab305bcc0e6f6619808
ariel almelda <arielalmelda@SnL-aalmelda.ameeratel.net>
2018-02-23 18:45:59 +0800

    02-23-2018

--- src/index.js
+++ src/index.js
@@ -2,27 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const store = createStore(
-    rootReducer,
-    composeWithDevTools(
-        applyMiddleware(thunk)
-    )
-);
-
 ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>, 
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
     document.getElementById('root')
 );
 registerServiceWorker();
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,89 @@
 import React from 'react';
 import { Component } from 'react';
+import Contact from '../crud/contact';
+
+const contacts = [
+    {
+        id: 1,
+        name: 'Ariel Almelda',
+        number: '09103546296',
+        email: 'arielalmelda858@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Zak Almelda',
+        number: '09101234567',
+        email: 'gukoson747@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Del Almelda',
+        number: '09107456123',
+        email: 'gukoson@gmail.com'
+    }
+];
+
+localStorage.setItem('contacts', JSON.stringify(contacts));
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            contacts: JSON.parse(localStorage.getItem('contacts'))
+        }
+        this.deleteContact = this.deleteContact.bind(this);
+
+    }
+    componentWillMount(){
+        const contacts = this.getContacts();
+        this.setState({ contacts });
+    }
+
+    getContacts(){
+       return this.state.contacts;
+    }
+
+    deleteContact(id){
+        const contacts = this.getContacts();
+
+        const findContact = contacts.filter( contact => {
+            return contact.id !== id;
+        } );
+
+        this.setState({ contacts: findContact });
+    }
+
     render(){
         return(
             <div>
-                <h1>This is home page!</h1>
-                <form>
-                    <input type="text" name="firstname" id="firstname" />
-                    <input type="text" name="lastname" id="lastname" />
-                    <input type="email" name="emailadd" id="emailadd" />
-                    <input type="submit" value="Submit" />
-                </form>
+                <h1>Contact List</h1>
+                <table className="table table-striped">
+                    <tbody>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Number</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                        {
+                            this.state.contacts.map(
+                                contact => {
+                                    return (
+                                        <Contact 
+                                            key={contact.id}
+                                            //name={contact.name}
+                                            //number={contact.number}
+                                            //email={contact.email}
+                                            {...contact}
+                                            deleteContact={this.deleteContact}
+                                        />
+                                    );
+                                }
+                            )
+                        }
+                    </tbody>
+                </table>
             </div>
         );
     }
